d77d5ebfcb3a48e34e037785c907917dbcc9e4b6
Piyush Gupta <piyushgupta21506@gmail.com>
2025-06-09 00:14:05 +0530

    game 2

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
-var cluster = "";
+var timer = 6;
+var score = 0;
+var HitNumber = 0;
 
-for(var i =0; i<=104 ; i++){
-    
-    cluster += `<div class="bubble">${Math.floor(Math.random()*10)}</div>`;
+
+function makeBubble() {
+  var cluster = "";
+  for (var i = 0; i <= 104; i++) {
+    cluster += `<div class="bubble">${Math.floor(Math.random() * 10)}</div>`;
+  }
+  document.querySelector(".pbtm").innerHTML = cluster;
 }
 
-document.querySelector('.pbtm').innerHTML= cluster;
+function runtimer() {
+  var countdown = setInterval(function () {
+    if(timer>0){
+        timer--;
+        document.querySelector(".timeInterval").textContent = timer;
+    }else{
+        clearInterval(countdown)
+        document.querySelector(".pbtm").innerHTML = `<div>Time Over</div>`;
+        document.querySelector(".pbtm").style = `font-weight: 600; font-size: 4vw`;
+    }
+    
+  }, 1000);
+}
 
+function hitting(){
+    HitNumber = Math.floor(Math.random()*10);
+    document.querySelector(".hitNum").textContent= HitNumber;
+    IncreaseScore()
+}
 
+function IncreaseScore(){
+    score += 10;
+    document.querySelector(".ScoreNum").textContent = score;
+}
+
+document.querySelector(".pbtm").addEventListener("click", function(dets){
+    var ClickedNum = Number(dets.target.textContent);
+    if(ClickedNum === HitNumber && timer>0){
+        IncreaseScore();
+        makeBubble();
+    }
+})
+
+runtimer();
+
+makeBubble();
+
+hitting();
